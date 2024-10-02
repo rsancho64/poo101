dd58c746d0ef0888271e266c0a154bbdd626a7bd
rsancho64 <rsancho@iesrioarba.es>
2024-10-02 08:33:51 +0200

    .

--- Mascota.py
+++ Mascota.py
@@ -1,32 +1,36 @@
-from typing import Any
+# from typing import Any
 
 class Mascota:
 
     # def __init__(self):
     #     print(self,": hola, mundo")
 
-    def __init__(self, nombre=None):
-        self.__nombre = nombre
-        # print(self, self.__nombre, ": hola")
-        print(self.__nombre, ": hola")
+    def __init__(self, especie="animal", nombre=None):
+        self.__especie = especie
+        self.nombre = nombre
+        print(f"soy un {self.__especie} y me llamo {self.nombre}")  # TODO: USE self__str__?
 
-    def __setattr__(self, name, value) -> None:
-        if name == "__nombre":
-            self.__nombre = value
+    # def __setattr__(self, name, value) -> None:
+    #     if name == "__nombre":
+    #         self.__nombre = value
 
-    def __str__(self) -> str:
-        return self.__nombre
+    # # TODO: vs __repr__? # SEE: https://jarroba.com/repr-y-str-de-python/
+    # def __str__(self) -> str: 
+    #     return "{0}".format(self.__nombre)
 
 if __name__== "__main__":
 
     m0 = Mascota()
-    m1 = Mascota("Pompa")
-    # print(m0 == m1)
-    # print(m0.__nombre) #inaccesible
-    # print(m1.__nombre) #inaccesible   
+    m1 = Mascota(especie="perro")
+    m3 = Mascota(especie="perro", nombre="Pompa")
+    m4 = Mascota(nombre="Firulais",especie="perrete")
+    
+    # accesible? que? # TODO: test
+    # print()
 
-    m2 = Mascota("Noche")
-    m3 = Mascota("Bamban")
+    # m2 = Mascota("Noche") # TODO: MAKE IT CATS
+    # m3 = Mascota("Bamban")
 
-    m1.__setattr__("__nombre", "Pompeta")
-    print(m1)
+    # TODO: 
+    # m1.__setattr__("__nombre", "Pompeta")
+    # print(m1)
